50730036161411f075f96b07fc5e94e0ece980eb
Eric Li <ericliharker@gmail.com>
2025-12-12 02:12:29 -0500

    added solution for leetcode 28 in java

---
+++ solutions/28 - Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Solution {
+    public int strStr(String haystack, String needle) {
+        // Time Complexity: O(N * M) because we loop through each char of the haystack once.
+        //                  Creating each substring takes O(M).
+        // Space Complexity: O(M) for the substrings.
+        int size = haystack.length() - needle.length();
+        for (int i = 0; i <= size; i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter haystack: ");
+        String haystack = scanner.nextLine();
+
+        System.out.print("Enter needle: ");
+        String needle = scanner.nextLine();
+
+        Solution solution = new Solution();
+        int result = solution.strStr(haystack, needle);
+
+        System.out.println("Output: " + result);
+    }
+}
