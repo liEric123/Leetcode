350da0dc58dfe1714654751feef8d76d48ca8107
Eric Li <81791793+liEric123@users.noreply.github.com>
2026-03-04 23:20:22 -0500

    added solution to leetcode 1758 in java

---
+++ solutions/1758 - Minimum Changes To Make Alternating Binary String.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public int minOperations(String s) {
+        int startZero = 0;
+        int startOne = 0;
+        boolean need0 = true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                if (need0) {
+                    startOne++;
+                } else {
+                    startZero++;
+                }
+            } else {
+                if (need0) {
+                    startZero++;
+                } else {
+                    startOne++;
+                }
+            }
+            need0 = !need0;
+        }
+        return Math.min(startOne, startZero);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the binary string:");
+        String s = sc.nextLine().trim();
+
+        Solution sol = new Solution();
+        int result = sol.minOperations(s);
+
+        System.out.println("Minimum operations:");
+        System.out.println(result);
+
+        sc.close();
+    }
+}
